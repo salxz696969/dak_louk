63e0063ea5cfdda96ab9f8a6f839e72d1c16e0dc
Chanraksa Ory <OCraksa@gmail.com>
2026-01-05 17:50:03 +0700

    removed warning

--- lib/ui/widgets/merchant/chats/chat_input.dart
+++ lib/ui/widgets/merchant/chats/chat_input.dart
@@ -1,5 +1,3 @@
-import 'package:dak_louk/domain/models/models.dart';
-import 'package:dak_louk/domain/services/merchant/chat_service.dart';
 import 'package:flutter/material.dart';
 
 class ChatInput extends StatelessWidget {
@@ -7,7 +5,6 @@ class ChatInput extends StatelessWidget {
 
   final Function(String) onSend;
 
-  final ChatService _chatService = ChatService();
   final TextEditingController _controller = TextEditingController();
 
   void handleSend(String text) {
