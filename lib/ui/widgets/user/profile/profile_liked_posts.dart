fec20803586dc52b03d05923ecb77afadf9f6547
Chanraksa Ory <OCraksa@gmail.com>
2026-01-07 02:07:43 +0700

    feat: navigate to post detail on profile & fix: similar posts

--- lib/ui/widgets/user/profile/profile_liked_posts.dart
+++ lib/ui/widgets/user/profile/profile_liked_posts.dart
@@ -1,4 +1,6 @@
 import 'package:dak_louk/domain/models/models.dart';
+import 'package:dak_louk/ui/screens/user/post_detail_screen.dart';
+import 'package:dak_louk/ui/widgets/user/profile/profile_post_item.dart';
 import 'package:flutter/material.dart';
 
 class LikedPosts extends StatelessWidget {
@@ -22,52 +24,8 @@ class LikedPosts extends StatelessWidget {
       itemCount: likedPosts.length,
       itemBuilder: (context, index) {
         final post = likedPosts[index];
-        return PhotoContainer(post: post);
+        return ProfilePostItem(post: post);
       },
-    );
-  }
-}
-
-class PhotoContainer extends StatelessWidget {
-  final UserProfileLikedSavedPostsVM post;
-
-  const PhotoContainer({required this.post});
-
-  @override
-  Widget build(BuildContext context) {
-    return InkWell(
-      // onTap: () {
-      //   Navigator.push(
-      //     context,
-      //     MaterialPageRoute(
-      //       builder: (context) => ProductInfoScreen(post: post),
-      //     ),
-      //   );
-      // },
-      child: Stack(
-        children: [
-          Positioned.fill(
-            child: Image(image: AssetImage(post.imageUrl), fit: BoxFit.cover),
-          ),
-          if (post.imageUrl.isNotEmpty)
-            Positioned(
-              top: 8,
-              right: 8,
-              child: Container(
-                padding: const EdgeInsets.all(6),
-                decoration: BoxDecoration(
-                  color: Colors.black54,
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                child: const Icon(
-                  Icons.collections,
-                  color: Colors.white,
-                  size: 20,
-                ),
-              ),
-            ),
-        ],
-      ),
     );
   }
 }
